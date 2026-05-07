3babc54e28ebe5326955d1b53146199b1cc23d10
VAN HELDEN Jacques <Jacques.van-Helden@univ-amu.fr>
2026-05-07 10:24:59 +0200

    replaced the test sequences in test_read_fasta() to include differences of lengths and sequences

--- tests/test_sequences.py
+++ tests/test_sequences.py
@@ -3,14 +3,14 @@ from bioseq_kmers.sequences import read_fasta
 
 def test_read_fasta(tmp_path):
     fasta = tmp_path / "test.fasta"
-    fasta.write_text(">seq1\nACGTACGN\n"
+    fasta.write_text(">seq1\nACGGNA\n"
                      ">seq2\nACGTACGN\n"
-                     ">seq3\nACGTACGN\n")
+                     ">seq3\nAAAAAA\n")
 
     sequences, total_length, seq_number = read_fasta(fasta)
 
-    assert sequences == {"seq1": "ACGTACGN",
+    assert sequences == {"seq1": "ACGGNA",
                          "seq2": "ACGTACGN",
-                         "seq3": "ACGTACGN"}
-    assert total_length == 24
+                         "seq3": "AAAAAA"}
+    assert total_length == 20
     assert seq_number == 3
