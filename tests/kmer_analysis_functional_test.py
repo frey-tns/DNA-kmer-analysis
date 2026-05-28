8d642214100b64d5b878da89a3155adb8b04502d
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2026-05-28 22:04:37 +0200

    Added oligo-analysis command
    implemented `test_oligo_analysis_markov_from_input` to compute k-mer statistics with RSAT oligo-analysis

--- tests/kmer_analysis_functional_test.py
+++ tests/kmer_analysis_functional_test.py
@@ -167,3 +167,112 @@ def test_kmer_analysis_markov_from_input(markov_order):
     # ------------------------------------------------------------------
 
     assert filtered_tsv.stat().st_size > 0
+
+
+@pytest.mark.parametrize("markov_order", range(1, 5))
+def test_oligo_analysis_markov_from_input(markov_order):
+    # ------------------------------------------------------------------
+    # Input / output files
+    # ------------------------------------------------------------------
+
+
+    output_dir = Path("results/oligo-analysis-markov-series/bg-from-input")
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    input_fasta = "data/seq/yeast_MET_upstream.fasta"
+
+
+    output_tsv = (
+        output_dir /
+        f"oligos_yeast_MET_upstream_k6_mkv{markov_order}_bginput_enriched.tsv"
+    )
+
+    # ------------------------------------------------------------------
+    # Command 1 : oligo-analysis
+    # ------------------------------------------------------------------
+
+    cmd1 = [
+        "source ~/packages/rsat/RSAT_config.bashrc && "
+        "rsat oligo-analysis -v 1"
+        f" -i {input_fasta}"
+        " -format fasta"
+        " -l 6"
+        " -2str"
+        f" -markov {markov_order}"
+        " -return freq,occ,proba,rank"
+        " -seqtype dna"
+        " -pseudo 0"
+        f" -o {output_tsv}"
+    ]
+
+    print("\nCommand:\n" + " ".join(cmd1))
+
+    result1 = subprocess.run(
+        cmd1,
+        shell=True,
+        executable="/bin/bash",
+        capture_output=True,
+        text=True,
+    )
+
+    print(result1.stdout)
+    print(result1.stderr)
+
+#    assert result1.returncode == 0
+    assert output_tsv.exists()
+
+@pytest.mark.parametrize("markov_order", range(1, 5))
+def test_oligo_analysis_markov_from_file(markov_order):
+    # ------------------------------------------------------------------
+    # Input / output files
+    # ------------------------------------------------------------------
+
+
+    output_dir = Path("results/oligo-analysis-markov-series/bg-from-file")
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    input_fasta = "data/seq/yeast_MET_upstream.fasta"
+
+    background_model = (
+        f"data/bg-models/"
+        f"yeast_all-upstream-noorf_mkv{markov_order}.tsv"
+    )
+
+    output_tsv = (
+        output_dir /
+        f"oligos_yeast_MET_upstream_k6_mkv{markov_order}_bginput_enriched.tsv"
+    )
+
+    # ------------------------------------------------------------------
+    # Command 1 : oligo-analysis
+    # ------------------------------------------------------------------
+
+    cmd1 = [
+        "source ~/packages/rsat/RSAT_config.bashrc && "
+        "rsat oligo-analysis -v 1"
+        f" -i {input_fasta}"
+        " -format fasta"
+        " -l 6"
+        " -2str"
+        f" -markov {markov_order}"
+        " -return freq,occ,proba,rank"
+        " -seqtype dna"
+        " -pseudo 0"
+        f" -o {output_tsv}"
+    ]
+
+    print("\nCommand:\n" + " ".join(cmd1))
+
+    result1 = subprocess.run(
+        cmd1,
+        shell=True,
+        executable="/bin/bash",
+        capture_output=True,
+        text=True,
+    )
+
+    print(result1.stdout)
+    print(result1.stderr)
+
+#    assert result1.returncode == 0
+    assert output_tsv.exists()
